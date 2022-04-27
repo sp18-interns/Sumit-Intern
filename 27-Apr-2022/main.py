2ac82d1628a1a0f94c1df6a57975cd78295ad0ac
devopssumit <sumit.shukla@sparkeighteen.com>
2022-04-27 11:17:31 +0530

    Assignment code

---
+++ 27-Apr-2022/main.py
@@ -0,0 +1,15 @@
+'''First thing first in oops'''
+'''We have to start with class , Class is Blueprint in any program which follow OOPS'''
+class Elevator:
+    def __init__(self, idle, num_of_floors, direction, cur_floor):  # Instantiate all attributes
+        self.num_of_floors = 6              # Instantiate object
+        self.cur_floor = cur_floor
+        self.direct = direction
+        self.idle = idle
+
+    def __str__(self):    # This method is to print object as string
+        print ("Wellcome to air_lifters")
+        return f"{self.num_of_floors}_{self.cur_floor}"
+
+Building = Elevator(0, 0 , "up",2)
+print(Building)
